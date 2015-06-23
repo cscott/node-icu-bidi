3d79e48f2588f6a83233ddbac847180ce8601742
C. Scott Ananian <cscott@cscott.net>
2015-06-23 10:28:08 -0400

    Fix building release binaries on iojs.
    Fix borrowed from
    https://github.com/mapbox/node-sqlite3/blob/master/scripts/build_against_node.sh

--- scripts/publish_x86.sh
+++ scripts/publish_x86.sh
@@ -18,10 +18,19 @@ sudo apt-get -y install gcc-multilib g++-multilib
 # node v0.8 and above provide pre-built 32 bit and 64 bit binaries
 # so here we use the 32 bit ones to also test 32 bit builds
 NVER=`node -v`
-wget http://nodejs.org/dist/${NVER}/node-${NVER}-${platform}-x86.tar.gz
-tar xf node-${NVER}-${platform}-x86.tar.gz
 # enable 32 bit node
-export PATH=$(pwd)/node-${NVER}-${platform}-x86/bin:$PATH
+if [[ ${NODE_VERSION:0:4} == 'iojs' ]]; then
+    wget https://iojs.org/download/release/${NVER}/iojs-${NVER}-${platform}-x86.tar.gz
+    tar xf iojs-${NVER}-${platform}-x86.tar.gz
+    # enable 32 bit iojs
+    export PATH=$(pwd)/iojs-${NVER}-${platform}-x86/bin:$PATH
+else
+    wget http://nodejs.org/dist/${NVER}/node-${NVER}-${platform}-x86.tar.gz
+    tar xf node-${NVER}-${platform}-x86.tar.gz
+    # enable 32 bit node
+    export PATH=$(pwd)/node-${NVER}-${platform}-x86/bin:$PATH
+fi
+node -e "console.log(process.arch,process.execPath)"
 # install 32 bit compiler toolchain and X11
 # test source compile in 32 bit mode
 CC=gcc-4.6 CXX=g++-4.6 npm install --build-from-source
